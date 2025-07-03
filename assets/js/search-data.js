a52d403255770a76f4c4177937e5ab2a85870e7b
jgmcm <james.mchugh@manchester.ac.uk>
2025-07-03 14:42:58 +0000

    Deploying to gh-pages from @ jgmcm/jgmcm.github.io@865d538c02d7a0cb4bd2e6a4016d00ffa8906459 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,18 +412,17 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-arxiv-preprint-sub-nm-curvature-unlocks-quantum-flexoelectricity-in-graphene",
+          title: 'ArXiv preprint – Sub-nm Curvature Unlocks Quantum Flexoelectricity in Graphene',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_1/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
